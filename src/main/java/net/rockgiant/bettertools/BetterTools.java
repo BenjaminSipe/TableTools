d685a44b5b4f9d64a2fcd5f76dd222dd3c198c37
Benjamin Max Sipe <sipe.nation3@gmail.com>
2023-10-11 18:50:57 -0500

    Initial commit

---
+++ src/main/java/net/rockgiant/bettertools/BetterTools.java
@@ -0,0 +1,18 @@
+package net.rockgiant.bettertools;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BetterTools implements ModInitializer {
+
+	public static final String MOD_ID = "bettertools";
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
