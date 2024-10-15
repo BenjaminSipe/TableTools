a482ba5abeee06c214aa884c9878613d20d2e843
Benjamin Sipe <sipe.nation3@gmail.com>
2024-10-14 21:46:42 -0500

    Rolled back solo attempt, will work from tutorial first

--- src/main/java/org/bsipe/btools/ModBlocks.java
+++ src/main/java/org/bsipe/btools/ModBlocks.java
@@ -4,23 +4,21 @@ import net.minecraft.block.*;
 import net.minecraft.block.enums.NoteBlockInstrument;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
-import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
-import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.intprovider.UniformIntProvider;
-import org.bsipe.btools.block.DeepslateForgeBlockEntityBlock;
+import org.bsipe.btools.block.DeepslateForgeBlock;
 
 import static org.bsipe.btools.BetterToolsModInitializer.MOD_ID;
 
 public class ModBlocks {
 
     public static void initialize() {
-        
+
     }
 
-    public static final DeepslateForgeBlockEntityBlock DEEPSLATE_FORGE = registerWithItem( "deepslate_forge", new DeepslateForgeBlockEntityBlock( AbstractBlock.Settings.create().strength( 1.5f, 6.0f).requiresTool()), new Item.Settings());
+//    public static final DeepslateForgeBlock DEEPSLATE_FORGE = registerWithItem( "deepslate_forge", new DeepslateForgeBlock( AbstractBlock.Settings.create().strength( 1.5f, 6.0f).requiresTool()), new Item.Settings());
     public static final Block ENDSTONE_ENDIRIUM_ORE = registerWithItem("endstone_endirium_ore", new ExperienceDroppingBlock(UniformIntProvider.create(3, 7), AbstractBlock.Settings.create().mapColor(MapColor.PALE_YELLOW).instrument(NoteBlockInstrument.BASEDRUM).requiresTool().strength(3.0F, 9.0F).luminance(Blocks.createLightLevelFromLitBlockState(10))), new Item.Settings() );
 
 
